685ca5516eb8632dc44be4161b7e24d7bd734e58
Tomer Harari <tharari21@gmail.com>
2022-08-18 10:30:55 -0400

    commit tree 59cdd717eb0065b4e33b7b75ae39d6e1e3e6b134

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,19 @@
+puts "Seeding API Data"
 # Add seed data in this file
+spells = ["acid-arrow", "animal-messenger", "calm-emotions", "charm-person"]
+
+spells.each do |spell|
+
+    response = RestClient.get("https://www.dnd5eapi.co/api/spells/#{spell}")
+
+    spell_hash = JSON.parse(response)
+
+    Spell.create(
+        name: spell_hash["name"],
+        level: spell_hash["level"],
+        description: spell_hash["desc"][0] # spell_hash["desc"] returns an array, so we need to access the first index to get just a string of the description
+    )
+
+end
+
+puts "Done seeding"
